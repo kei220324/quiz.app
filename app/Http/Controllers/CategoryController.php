59d6b02cad96b114726138216206f7fe582a372a
kei220324 <0vx05845724272k@ezweb.ne.jp>
2025-10-11 08:23:57 +0900

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    /**
+     * // カテゴリー新規登録処理
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        $category=new Category();
+        $category->name=$request->name;
+        $category->description=$request->description;
+        $category->save();
+        return  redirect()->route('admin.top');
+}
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+//管理画面トップページ
+// 管理画面
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('top', function() {
+        return view('admin.top');
+    })->name('top');
+
+    // カテゴリー管理
+    Route::prefix('categories')->name('categories.')->group(function () {
+        Route::get('create',[CategoryController::class,'create'])->name('create');
+        //かげゴリー新規登録処理
+        Route::post('store',[CategoryController::class,'store'])->name('store');
+    });
+});
+
+
+
+
